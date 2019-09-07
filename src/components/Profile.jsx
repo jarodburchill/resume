7f39bd17e1d802a7e35e80ccba88228ddecd733c
jarod <jarodburchill@gmail.com>
2019-09-06 22:52:54 -0400

    Stying changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import styles from "./styles/Profile.module.scss";
 import data from "../resume-data.json";
 
 function Profile() {
   const profile = data.profile;
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>{profile.name}</h1>
       <h2>{profile.profession}</h2>
     </div>
