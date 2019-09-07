52c5849c39edf6017d9a0c0a1786cec455667c2e
jarod <jarodburchill@gmail.com>
2019-09-07 00:26:03 -0400

    Finished skills data

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,14 +3,30 @@ import styles from "./styles/Skills.module.scss";
 import data from "../resume-data.json";
 
 function Skills() {
-  const frontend = data.skills.frontend;
+  const skills = data.skills;
+  const frontend = skills.frontend;
+  const backend = skills.backend;
+  const database = skills.database;
+  const other = skills.other;
 
   return (
     <div className={styles.container}>
       <h2>Skills</h2>
       <hr />
-      <h3>Front-end</h3>
+      <h3 className={styles.groupName}>Front-end</h3>
       {frontend.map((skill, index) => {
+        return <p key={index}>{skill}</p>;
+      })}
+      <h3 className={styles.groupName}>Back-end</h3>
+      {backend.map((skill, index) => {
+        return <p key={index}>{skill}</p>;
+      })}
+      <h3 className={styles.groupName}>Database</h3>
+      {database.map((skill, index) => {
+        return <p key={index}>{skill}</p>;
+      })}
+      <h3 className={styles.groupName}>Other</h3>
+      {other.map((skill, index) => {
         return <p key={index}>{skill}</p>;
       })}
     </div>
