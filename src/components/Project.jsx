a3a98024a17b916b61d1de6a17eaffc12a54e9de
jarod <jarodburchill@gmail.com>
2019-09-07 21:15:33 -0400

    Project Icon implementation

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,10 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import star from "../assets/star.svg";
+import fork from "../assets/fork.svg";
 
 function Project({ project }) {
+  const [starCount, setStarCount] = useState(0);
+  const [forkCount, setForkCount] = useState(0);
+
+  useEffect(() => {
+    getRepoStats();
+  });
+
+  const getRepoStats = async () => {
+    const response = await fetch(project.api);
+    const data = await response.json();
+    setStarCount(data.stargazers_count);
+    setForkCount(data.forks_count);
+  };
+
   return (
     <>
       <div className="global-header">
-        <h4>{project.title}</h4>
+        <div>
+          <h4>{project.title}</h4>
+          <span className="star">
+            <p>{starCount}</p>
+            <img src={star} alt="GitHub Stars" />
+          </span>
+          <span className="fork">
+            <p>{forkCount}</p>
+            <img src={fork} alt="GitHub Forks" />
+          </span>
+        </div>
         <p>{project.startDate}</p>
       </div>
       <div className="global-description">
